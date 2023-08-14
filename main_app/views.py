943c72d87c80a0faeb088eb31153eaad97b224c2
Brent <brentmueller32@gmail.com>
2023-08-14 15:13:27 -0700

    models

--- main_app/views.py
+++ main_app/views.py
@@ -17,6 +17,6 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', {
+  return render(request, 'finches/details.html', {
     'finch': finch
   })
