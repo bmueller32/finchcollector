11d8a7cfaf586ecef3a52267666ec16e5773bd50
Brent <brentmueller32@gmail.com>
2023-08-15 16:59:44 -0700

    add-edit-remove-feed

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,43 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
 
 # Create your models here.
 class Finch(models.Model):
-	# define the column names, and the dataypes in each row
-	name = models.CharField(max_length=100)
-	breed = models.CharField(max_length=100)
-	description = models.TextField(max_length=250)
-	age = models.IntegerField()
-	# CharField, TextField, IntegerField - these are called Field Types in Django 
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+
+  # Changing this instance method
+  # does not impact the database, therefore
+  # no makemigrations is necessary
+  def __str__(self):
+    return f'{self.name} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  # Create a finch_id FK
+  finch = models.ForeignKey(
+    Finch,
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -17,6 +17,37 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+   # instantiate FeedingForm to be rendered in detail.html
+  feeding_form = FeedingForm()
   return render(request, 'finches/details.html', {
-    'finch': finch
+    'finch': finch, 'feeding_form': feeding_form
   })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
+
+
+    
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using 
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    # because we have not assigned the
+    # finch_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
